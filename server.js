60da0060c410857879011f4541e887b5aa92ace3
Glitch (caring-sausage) <none>
2018-12-15 15:19:50 +0000

    🎢🕶 Checkpoint ./server.js:2505047/3

--- server.js
+++ server.js
@@ -43,6 +43,9 @@ app.use((err, req, res, next) => {
     .send(errMessage)
 })
 
+
+
+
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
